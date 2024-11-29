fdc440d2d488594f1568dad1f90e7d8d39cc11a4
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-11-29 17:49:50 +0200

    Update main.cpp

--- hw8/src/main.cpp
+++ hw8/src/main.cpp
@@ -1,5 +1,6 @@
 #include "SharedPtr.hpp"
 #include <iostream>
+#include <unordered_set>
 
 int main() {
     MySharedPtr<int> ptr1(new int(42));
@@ -14,5 +15,21 @@ int main() {
     }
 
     std::cout << "After ptr2 is out of scope, use_count: " << ptr1.use_count() << "\n";
+
+    MySharedPtr<int> ptr3 = std::move(ptr1);
+    std::cout << "After move, ptr3 use_count: " << ptr3.use_count() << "\n";
+    std::cout << "After move, ptr1 use_count: " << ptr1.use_count() << "\n";
+
+    std::unordered_set<MySharedPtr<int>, MySharedPtr<int>::Hasher> mySet;
+    mySet.insert(ptr3);
+    mySet.insert(MySharedPtr<int>(new int(200)));
+
+    for (const auto& elem : mySet) {
+        std::cout << "Set element value: " << *elem << ", use_count: " << elem.use_count() << "\n";
+    }
+
+    MySharedPtr<int> ptr4 = ptr3;
+    std::cout << "ptr3 == ptr4: " << (ptr3 == ptr4 ? "true" : "false") << "\n";
+
     return 0;
 }
