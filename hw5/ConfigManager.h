7802ab8e37a2846627ac1e58c049057cb26c8cd3
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-12-10 17:27:50 +0200

    Update ConfigManager.h

--- hw5/ConfigManager.h
+++ hw5/ConfigManager.h
@@ -5,18 +5,21 @@
 
 class ConfigManager {
 public:
-    static ConfigManager& getInstance() {
-        static ConfigManager instance;
+    static ConfigManager* getInstance() {
+        std::lock_guard<std::mutex> lock(mtx);
+        if (instance == nullptr) {
+            instance = new ConfigManager();
+        }
         return instance;
     }
 
     void setConfig(const std::string& key, const std::string& value) {
-        std::lock_guard<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock(configMutex);
         config_[key] = value;
     }
 
     std::string getConfig(const std::string& key) {
-        std::lock_guard<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock(configMutex);
         if (config_.find(key) != config_.end()) {
             return config_[key];
         } else {
@@ -31,5 +34,10 @@ private:
     ConfigManager& operator=(const ConfigManager&) = delete;
 
     std::unordered_map<std::string, std::string> config_;
-    std::mutex mutex_;
+    std::mutex configMutex;
+    static ConfigManager* instance;
+    static std::mutex mtx;
 };
+
+ConfigManager* ConfigManager::instance = nullptr;
+std::mutex ConfigManager::mtx;
