ef0cb2f89a756f68639eaff184d7500499e72d9a
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-12-10 17:28:05 +0200

    Update main.cpp

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -3,12 +3,12 @@
 #include "ConfigManager.h"
 
 int main() {
-    Logger::getInstance().log("This is an info message");
-    Logger::getInstance().log("This is a warning", Logger::Level::WARNING);
-    Logger::getInstance().log("This is an error", Logger::Level::ERROR);
+    Logger::getInstance()->log("This is an info message");
+    Logger::getInstance()->log("This is a warning", Logger::Level::WARNING);
+    Logger::getInstance()->log("This is an error", Logger::Level::ERROR);
 
-    ConfigManager::getInstance().setConfig("username", "admin");
-    std::cout << "Username: " << ConfigManager::getInstance().getConfig("username") << std::endl;
+    ConfigManager::getInstance()->setConfig("username", "admin");
+    std::cout << "Username: " << ConfigManager::getInstance()->getConfig("username") << std::endl;
 
     return 0;
 }
