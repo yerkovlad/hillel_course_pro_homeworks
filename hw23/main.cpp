072f3cdc0a873d018b2caaebb07c55a8f4b4bd04
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2025-02-04 13:09:40 +0200

    Update main.cpp

--- hw23/main.cpp
+++ hw23/main.cpp
@@ -1,1 +1,26 @@
-test
+#include <QApplication>
+#include <QTreeView>
+#include "CustomFileSystemModel.h"
+#include "CustomDelegate.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    CustomFileSystemModel *model = new CustomFileSystemModel;
+    model->setRootPath(QDir::homePath());
+    model->setFilter(QDir::NoDotAndDotDot | QDir::Files | QDir::Dirs);
+    model->setNameFilters(QStringList() << "*.txt" << "*.cpp");
+    model->setNameFilterDisables(false);
+
+    QTreeView *treeView = new QTreeView;
+    treeView->setModel(model);
+    treeView->setRootIndex(model->index(QDir::homePath()));
+    treeView->resize(800, 600);
+
+    CustomDelegate *delegate = new CustomDelegate(treeView);
+    treeView->setItemDelegate(delegate);
+
+    treeView->show();
+
+    return app.exec();
+}
